81057f2855a97cf0c76640c7de107fe490f57885
liam <liam.oshaughnessy@live.co.uk>
2022-12-13 13:58:55 +0000

    checkerboard arrays practical complete

--- practicals.py
+++ practicals.py
@@ -2,8 +2,10 @@ import numpy as np
 
 #Count the unique values in a row
 
-#random ints between 0 and 10 (so 9) 9 rows, 12 columns 
+#random ints between 0 and 10 (so 9) 9 columns, 12 rows 
 arr = np.random.randint(0, 10, size=(9,12))
+
+print(arr)
 
 def count_unique(ar):
     return len(np.unique(ar))
@@ -11,5 +13,6 @@ def count_unique(ar):
 for i in arr:
     print(count_unique(i))
     
+
 #--------------------------------------------------------
 
